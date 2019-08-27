df5d172651705a4648e5dbeecad4415f08dfa0e3
Sam Roberts <53512773+sam-roberts-bjss@users.noreply.github.com>
2019-08-27 10:55:38 +0100

    Task 2804 - Introduced the use of AutoMapper.
    This was to simplify the code to map from a domain model object to a view model object.

---
+++ tests/NHSD.BuyingCatalogue.Application.Tests/Infrastructure/AutoMapperFactory.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using NHSD.BuyingCatalogue.Application.Infrastructure.Mapping;
+
+namespace NHSD.BuyingCatalogue.Application.Tests.Infrastructure
+{
+	internal static class AutoMapperFactory
+	{
+		internal static IMapper Create()
+		{
+			var mappingConfiguration = new MapperConfiguration((mapperConfigurationExpression) =>
+			{
+				mapperConfigurationExpression.AddProfile(new AutoMapperProfile());
+			});
+
+			return mappingConfiguration.CreateMapper();
+		}
+	}
+}
