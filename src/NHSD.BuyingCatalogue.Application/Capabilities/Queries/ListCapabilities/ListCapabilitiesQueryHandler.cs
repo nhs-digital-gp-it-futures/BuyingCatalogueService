98651f1319d9d6b3a8efafe8d7dcb32ed619f2c0
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-08-28 15:10:44 +0100

    - make properties private by default

--- src/NHSD.BuyingCatalogue.Application/Capabilities/Queries/ListCapabilities/ListCapabilitiesQueryHandler.cs
+++ src/NHSD.BuyingCatalogue.Application/Capabilities/Queries/ListCapabilities/ListCapabilitiesQueryHandler.cs
@@ -8,41 +8,41 @@ using NHSD.BuyingCatalogue.Domain.Entities;
 
 namespace NHSD.BuyingCatalogue.Application.Capabilities.Queries.ListCapabilities
 {
-	public sealed class ListCapabilitiesQueryHandler : IRequestHandler<ListCapabilitiesQuery, ListCapabilitiesQueryResult>
-	{
-		/// <summary>
-		/// Data access layer for the <see cref="Capability"/> entity.
-		/// </summary>
-		public ICapabilityRepository CapabilityRepository { get; }
+    public sealed class ListCapabilitiesQueryHandler : IRequestHandler<ListCapabilitiesQuery, ListCapabilitiesQueryResult>
+    {
+        /// <summary>
+        /// Data access layer for the <see cref="Capability"/> entity.
+        /// </summary>
+        private ICapabilityRepository CapabilityRepository { get; }
 
-		/// <summary>
-		/// The mapper to convert one object into another type.
-		/// </summary>
-		public IMapper Mapper { get; }
+        /// <summary>
+        /// The mapper to convert one object into another type.
+        /// </summary>
+        private IMapper Mapper { get; }
 
-		/// <summary>
-		/// Initialises a new instance of the <see cref="ListCapabilitiesQueryHandler"/> class.
-		/// </summary>
-		public ListCapabilitiesQueryHandler(ICapabilityRepository capabilityRepository, IMapper mapper)
-		{
-			CapabilityRepository = capabilityRepository ?? throw new System.ArgumentNullException(nameof(capabilityRepository));
-			Mapper = mapper ?? throw new System.ArgumentNullException(nameof(mapper));
-		}
+        /// <summary>
+        /// Initialises a new instance of the <see cref="ListCapabilitiesQueryHandler"/> class.
+        /// </summary>
+        public ListCapabilitiesQueryHandler(ICapabilityRepository capabilityRepository, IMapper mapper)
+        {
+            CapabilityRepository = capabilityRepository ?? throw new System.ArgumentNullException(nameof(capabilityRepository));
+            Mapper = mapper ?? throw new System.ArgumentNullException(nameof(mapper));
+        }
 
-		/// <summary>
-		/// Gets the query result.
-		/// </summary>
-		/// <param name="request">The query parameters.</param>
-		/// <param name="cancellationToken">Token to cancel the request.</param>
-		/// <returns>The result of the query.</returns>
-		public async Task<ListCapabilitiesQueryResult> Handle(ListCapabilitiesQuery request, CancellationToken cancellationToken)
-		{
-			IEnumerable<Capability> capabilities = await CapabilityRepository.ListAsync(cancellationToken).ConfigureAwait(false);
+        /// <summary>
+        /// Gets the query result.
+        /// </summary>
+        /// <param name="request">The query parameters.</param>
+        /// <param name="cancellationToken">Token to cancel the request.</param>
+        /// <returns>The result of the query.</returns>
+        public async Task<ListCapabilitiesQueryResult> Handle(ListCapabilitiesQuery request, CancellationToken cancellationToken)
+        {
+            IEnumerable<Capability> capabilities = await CapabilityRepository.ListAsync(cancellationToken).ConfigureAwait(false);
 
-			return new ListCapabilitiesQueryResult
-			{
-				Capabilities = Mapper.Map<IEnumerable<CapabilityViewModel>>(capabilities)
-			};
-		}
-	}
+            return new ListCapabilitiesQueryResult
+            {
+                Capabilities = Mapper.Map<IEnumerable<CapabilityViewModel>>(capabilities)
+            };
+        }
+    }
 }
