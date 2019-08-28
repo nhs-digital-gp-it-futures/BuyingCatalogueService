153863feeb8bf7b10291c187f5bf2bb15a6d53fb
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-08-28 16:15:00 +0100

    - make properties private by default

--- src/NHSD.BuyingCatalogue.Application/Solutions/Queries/GetAllSolutionSummaries/GetAllSolutionSummariesQueryHandler.cs
+++ src/NHSD.BuyingCatalogue.Application/Solutions/Queries/GetAllSolutionSummaries/GetAllSolutionSummariesQueryHandler.cs
@@ -10,44 +10,44 @@ using NHSD.BuyingCatalogue.Domain;
 
 namespace NHSD.BuyingCatalogue.Application.Solutions.Queries.GetAll
 {
-	/// <summary>
-	/// Defines the request handler for the <see cref="GetAllSolutionSummariesQuery"/>.
-	/// </summary>
-	public sealed class GetAllSolutionSummariesQueryHandler : IRequestHandler<GetAllSolutionSummariesQuery, GetAllSolutionSummariesQueryResult>
-	{
-		/// <summary>
-		/// Access the persistence layer for the <see cref="Solution"/> entity.
-		/// </summary>
-		public ISolutionRepository SolutionsRepository { get; }
+    /// <summary>
+    /// Defines the request handler for the <see cref="GetAllSolutionSummariesQuery"/>.
+    /// </summary>
+    public sealed class GetAllSolutionSummariesQueryHandler : IRequestHandler<GetAllSolutionSummariesQuery, GetAllSolutionSummariesQueryResult>
+    {
+        /// <summary>
+        /// Access the persistence layer for the <see cref="Solution"/> entity.
+        /// </summary>
+        private ISolutionRepository SolutionsRepository { get; }
 
-		/// <summary>
-		/// The mapper to convert one object into another type.
-		/// </summary>
-		public IMapper Mapper { get; }
+        /// <summary>
+        /// The mapper to convert one object into another type.
+        /// </summary>
+        private IMapper Mapper { get; }
 
-		/// <summary>
-		/// Initialises a new instance of the <see cref="GetAllSolutionSummariesQueryHandler"/> class.
-		/// </summary>
-		public GetAllSolutionSummariesQueryHandler(ISolutionRepository solutionsRepository, IMapper mapper)
-		{
-			SolutionsRepository = solutionsRepository ?? throw new ArgumentNullException(nameof(solutionsRepository));
-			Mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
-		}
+        /// <summary>
+        /// Initialises a new instance of the <see cref="GetAllSolutionSummariesQueryHandler"/> class.
+        /// </summary>
+        public GetAllSolutionSummariesQueryHandler(ISolutionRepository solutionsRepository, IMapper mapper)
+        {
+            SolutionsRepository = solutionsRepository ?? throw new ArgumentNullException(nameof(solutionsRepository));
+            Mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
 
-		/// <summary>
-		/// Gets the query result.
-		/// </summary>
-		/// <param name="request">The query parameters.</param>
-		/// <param name="cancellationToken">Token to cancel the request.</param>
-		/// <returns>The result of the query.</returns>
-		public async Task<GetAllSolutionSummariesQueryResult> Handle(GetAllSolutionSummariesQuery request, CancellationToken cancellationToken)
-		{
-			IEnumerable<Solution> solutionList = await SolutionsRepository.ListSolutionSummaryAsync(cancellationToken).ConfigureAwait(false);
+        /// <summary>
+        /// Gets the query result.
+        /// </summary>
+        /// <param name="request">The query parameters.</param>
+        /// <param name="cancellationToken">Token to cancel the request.</param>
+        /// <returns>The result of the query.</returns>
+        public async Task<GetAllSolutionSummariesQueryResult> Handle(GetAllSolutionSummariesQuery request, CancellationToken cancellationToken)
+        {
+            IEnumerable<Solution> solutionList = await SolutionsRepository.ListSolutionSummaryAsync(cancellationToken).ConfigureAwait(false);
 
-			return new GetAllSolutionSummariesQueryResult
-			{
-				Solutions = Mapper.Map<IEnumerable<SolutionSummaryViewModel>>(solutionList.Where(item => item != null && item.Organisation != null && item.Capabilities != null && item.Capabilities.Any()))
-			};
-		}
-	}
+            return new GetAllSolutionSummariesQueryResult
+            {
+                Solutions = Mapper.Map<IEnumerable<SolutionSummaryViewModel>>(solutionList.Where(item => item != null && item.Organisation != null && item.Capabilities != null && item.Capabilities.Any()))
+            };
+        }
+    }
 }
