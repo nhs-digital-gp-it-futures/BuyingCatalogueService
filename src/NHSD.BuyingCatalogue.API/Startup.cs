2058ebd1c0ce4160690d35ee3f3040454e4ff9e9
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-08-29 16:08:09 +0100

    - type is in same assy

--- src/NHSD.BuyingCatalogue.API/Startup.cs
+++ src/NHSD.BuyingCatalogue.API/Startup.cs
@@ -13,75 +13,74 @@ using System;
 
 namespace NHSD.BuyingCatalogue.API
 {
-  /// <summary>
-  /// Represents a boostrapper for the application. Used as a starting point to configure the API.
-  /// </summary>
-  public sealed class Startup
-  {
     /// <summary>
-    /// Application configuration.
+    /// Represents a boostrapper for the application. Used as a starting point to configure the API.
     /// </summary>
-    private IConfiguration Configuration { get; }
-
-    /// <summary>
-    /// Initialises a new instance of the <see cref="Startup"/> class.
-    /// </summary>
-    public Startup(IConfiguration configuration)
+    public sealed class Startup
     {
-      Configuration = configuration;
+        /// <summary>
+        /// Application configuration.
+        /// </summary>
+        private IConfiguration Configuration { get; }
 
-      DumpSettings();
+        /// <summary>
+        /// Initialises a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+            DumpSettings();
+        }
+
+        /// <summary>
+        /// Configures the services for the application.
+        /// </summary>
+        /// <param name="services">The collection of services.</param>
+        /// <remarks>This method gets called by the runtime. Use this method to add services to the container.</remarks>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+              .AddAutoMapper(typeof(AutoMapperProfile).Assembly)
+              .AddCustomDbFactory()
+              .AddCustomRepositories()
+              .AddMediatR(typeof(GetAllSolutionSummariesQueryHandler).Assembly)
+              .AddCustomSwagger()
+              .AddCustomMvc();
+        }
+
+        /// <summary>
+        /// Configures the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">The application builder.</param>
+        /// <param name="env">The hosting environment details.</param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger()
+                   .UseSwaggerUI(options =>
+                  {
+                      options.SwaggerEndpoint("/swagger/v1/swagger.json", "Buying Catalog API V1");
+                  });
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            //TODO : Restore HTTPS
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+
+        private void DumpSettings()
+        {
+            Console.WriteLine("Settings:");
+            Console.WriteLine($"  CONNECTIONSTRINGS:");
+            Console.WriteLine($"    BUYINGCATALOGUE: {Configuration.BuyingCatalogueConnectionString()}");
+        }
     }
-
-    /// <summary>
-    /// Configures the services for the application.
-    /// </summary>
-    /// <param name="services">The collection of services.</param>
-    /// <remarks>This method gets called by the runtime. Use this method to add services to the container.</remarks>
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services
-        .AddAutoMapper(typeof(AutoMapperProfile).Assembly)
-        .AddCustomDbFactory()
-        .AddCustomRepositories()
-        .AddMediatR(typeof(GetAllSolutionSummariesQueryHandler).Assembly)
-              .AddMediatR(typeof(GetSolutionByIdHandler).Assembly)
-        .AddCustomSwagger()
-        .AddCustomMvc();
-    }
-
-    /// <summary>
-    /// Configures the HTTP request pipeline.
-    /// </summary>
-    /// <param name="app">The application builder.</param>
-    /// <param name="env">The hosting environment details.</param>
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-        app.UseSwagger()
-           .UseSwaggerUI(options =>
-          {
-            options.SwaggerEndpoint("/swagger/v1/swagger.json", "Buying Catalog API V1");
-          });
-      }
-      else
-      {
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-      }
-
-      //TODO : Restore HTTPS
-      //app.UseHttpsRedirection();
-      app.UseMvc();
-    }
-
-    private void DumpSettings()
-    {
-        Console.WriteLine("Settings:");
-        Console.WriteLine($"  CONNECTIONSTRINGS:");
-        Console.WriteLine($"    BUYINGCATALOGUE: {Configuration.BuyingCatalogueConnectionString()}");
-    }
-  }
 }
