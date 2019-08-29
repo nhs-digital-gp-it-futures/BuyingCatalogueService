384f8d9519c6da6f7c285f35969a55b7a6dbeb7b
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-08-29 12:26:22 +0100

    - make sure we're mapping what we expect

---
+++ tests/NHSD.BuyingCatalogue.Application.UnitTests/Infrastructure/Mapping/AutoMapperProfile_Tests.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper.Configuration;
+using NHSD.BuyingCatalogue.Application.Capabilities.Queries.ListCapabilities;
+using NHSD.BuyingCatalogue.Application.Infrastructure.Mapping;
+using NHSD.BuyingCatalogue.Application.Solutions.Queries.GetAll;
+using NHSD.BuyingCatalogue.Domain;
+using NHSD.BuyingCatalogue.Domain.Entities;
+using NUnit.Framework;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NHSD.BuyingCatalogue.Application.UnitTests.Infrastructure.Mapping
+{
+    [TestFixture]
+    public sealed class AutoMapperProfile_Tests
+    {
+        private AutoMapperProfile _profile;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _profile = new AutoMapperProfile();
+        }
+
+        public static IEnumerable<KeyValuePair<Type, Type>> SupportedMappings()
+        {
+            yield return new KeyValuePair<Type, Type>(typeof(Organisation), typeof(SolutionOrganisationViewModel));
+            yield return new KeyValuePair<Type, Type>(typeof(Solution), typeof(SolutionSummaryViewModel));
+            yield return new KeyValuePair<Type, Type>(typeof(Capability), typeof(SolutionCapabilityViewModel));
+            yield return new KeyValuePair<Type, Type>(typeof(Capability), typeof(CapabilityViewModel));
+        }
+
+        [Test]
+        public void Profile_MatchesMapping()
+        {
+            var configs = ((IProfileConfiguration)_profile).TypeMapConfigs;
+
+            var mappings = configs.Select(tmc => new KeyValuePair<Type, Type>(tmc.SourceType, tmc.DestinationType));
+
+            SupportedMappings().ShouldBe(mappings, ignoreOrder: true);
+        }
+    }
+}
