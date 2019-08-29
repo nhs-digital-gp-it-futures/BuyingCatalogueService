47b1f1f538ba17b84edfda1ff40011318f39bf2f
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-08-29 09:38:44 +0100

    - refactored db connection string
    - output db connection string for diagnostic purposes

---
+++ src/NHSD.BuyingCatalogue.Application/Infrastructure/Settings.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace NHSD.BuyingCatalogue.Application.Infrastructure
+{
+    public static class Settings
+    {
+        public static string BuyingCatalogueConnectionString(this IConfiguration config)
+        {
+            return config["ConnectionStrings:BuyingCatalogue"];
+        }
+    }
+}
--- src/NHSD.BuyingCatalogue.Persistence/Infrastructure/DbConnectionFactory.cs
+++ src/NHSD.BuyingCatalogue.Persistence/Infrastructure/DbConnectionFactory.cs
@@ -4,41 +4,42 @@ using System.Data.SqlClient;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using NHSD.BuyingCatalogue.Application.Infrastructure;
 
 namespace NHSD.BuyingCatalogue.Persistence.Infrastructure
 {
-	/// <summary>
-	/// A factory to provide a new database connection.
-	/// </summary>
-	public sealed class DbConnectionFactory : IDbConnectionFactory
-	{
-		public IConfiguration Configuration { get; }
+  /// <summary>
+  /// A factory to provide a new database connection.
+  /// </summary>
+  public sealed class DbConnectionFactory : IDbConnectionFactory
+  {
+    private IConfiguration Configuration { get; }
 
-		/// <summary>
-		/// Gets the database connection details.
-		/// </summary>
-		public string ConnectionString => Configuration.GetConnectionString("BuyingCatalogue");
+    /// <summary>
+    /// Gets the database connection details.
+    /// </summary>
+    private string ConnectionString => Configuration.BuyingCatalogueConnectionString();
 
-		/// <summary>
-		/// Initialises a new instance of the <see cref="DbConnectionFactory"/> class.
-		/// </summary>
-		public DbConnectionFactory(IConfiguration configuration)
-		{
-			Configuration = configuration ?? throw new System.ArgumentNullException(nameof(configuration));
-		}
+    /// <summary>
+    /// Initialises a new instance of the <see cref="DbConnectionFactory"/> class.
+    /// </summary>
+    public DbConnectionFactory(IConfiguration configuration)
+    {
+      Configuration = configuration ?? throw new System.ArgumentNullException(nameof(configuration));
+    }
 
-		/// <summary>
-		/// Gets a new database connection.
-		/// </summary>
-		/// <returns>A new database connection.</returns>
-		public async Task<IDbConnection> GetAsync(CancellationToken cancellationToken)
-		{
-			DbConnection connection = SqlClientFactory.Instance.CreateConnection();
-			connection.ConnectionString = ConnectionString;
+    /// <summary>
+    /// Gets a new database connection.
+    /// </summary>
+    /// <returns>A new database connection.</returns>
+    public async Task<IDbConnection> GetAsync(CancellationToken cancellationToken)
+    {
+      DbConnection connection = SqlClientFactory.Instance.CreateConnection();
+      connection.ConnectionString = ConnectionString;
 
-			await connection.OpenAsync(cancellationToken);
+      await connection.OpenAsync(cancellationToken);
 
-			return connection;
-		}
-	}
+      return connection;
+    }
+  }
 }
