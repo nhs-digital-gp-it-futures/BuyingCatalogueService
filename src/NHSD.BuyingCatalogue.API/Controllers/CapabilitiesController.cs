19c98e44dbfb18359203027c5735db09427b7a52
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-09-04 15:56:45 +0100

    - allow access to public data

--- src/NHSD.BuyingCatalogue.API/Controllers/CapabilitiesController.cs
+++ src/NHSD.BuyingCatalogue.API/Controllers/CapabilitiesController.cs
@@ -1,39 +1,36 @@
-﻿using System;
-using System.Net;
-using System.Threading.Tasks;
-using MediatR;
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NHSD.BuyingCatalogue.Application.Capabilities.Queries.ListCapabilities;
+using System;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace NHSD.BuyingCatalogue.API.Controllers
 {
-	[Route("api/v1/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
-    public class CapabilitiesController : ControllerBase
+    [Produces("application/json")]
+    [AllowAnonymous]
+    public sealed class CapabilitiesController : ControllerBase
     {
-		/// <summary>
-		/// The mediator to pass commands and queries through.
-		/// </summary>
-		public IMediator Mediator { get; }
+        private readonly IMediator _mediator;
 
-		/// <summary>
-		/// Initialises a new instance of the <see cref="SolutionsSummaryController"/> class.
-		/// </summary>
-		public CapabilitiesController(IMediator mediator)
-		{
-			Mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
-		}
+        public CapabilitiesController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
 
-		/// <summary>
-		/// Gets a list of capabilitiies.
-		/// </summary>
-		/// <returns>A result containing a list of capabilities.</returns>
-		[HttpGet]
-		[ProducesResponseType(typeof(ListCapabilitiesQueryResult), (int)HttpStatusCode.OK)]
-		[ProducesResponseType((int)HttpStatusCode.BadRequest)]
-		public async Task<ActionResult<ListCapabilitiesQueryResult>> ListAsync()
-		{
-			return Ok(await Mediator.Send(new ListCapabilitiesQuery()));
-		}
-	}
+        /// <summary>
+        /// Gets a list of capabilitiies.
+        /// </summary>
+        /// <returns>A result containing a list of capabilities.</returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(ListCapabilitiesQueryResult), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult<ListCapabilitiesQueryResult>> ListAsync()
+        {
+            return Ok(await _mediator.Send(new ListCapabilitiesQuery()));
+        }
+    }
 }
