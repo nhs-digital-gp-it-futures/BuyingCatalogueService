e2a49e82d98970992aab9862c18d44c937a93658
Sam Roberts <53512773+sam-roberts-bjss@users.noreply.github.com>
2019-09-19 08:39:39 +0100

    Task 2984 - Added health checks to the API.
    This will allow any integration tests to verify the status of the application before executing any tests.
    It will also allow monitoring products to determine the ready status of the application.

---
+++ src/NHSD.BuyingCatalogue.API/Infrastructure/HealthChecks/HealthCheckTags.cs
@@ -0,0 +1,8 @@
+namespace NHSD.BuyingCatalogue.API.Infrastructure.HealthChecks
+{
+    public static class HealthCheckTags
+    {
+        public const string Live = "live";
+        public const string Dependencies = "dependencies";
+    }
+}
--- src/NHSD.BuyingCatalogue.Persistence/Infrastructure/DbConnectionFactory.cs
+++ src/NHSD.BuyingCatalogue.Persistence/Infrastructure/DbConnectionFactory.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+using System;
+using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
 using System.Threading;
@@ -8,38 +9,52 @@ using NHSD.BuyingCatalogue.Application.Infrastructure;
 
 namespace NHSD.BuyingCatalogue.Persistence.Infrastructure
 {
-  /// <summary>
-  /// A factory to provide a new database connection.
-  /// </summary>
-  public sealed class DbConnectionFactory : IDbConnectionFactory
-  {
-    private IConfiguration Configuration { get; }
-
     /// <summary>
-    /// Gets the database connection details.
+    /// A factory to provide a new database connection.
     /// </summary>
-    private string ConnectionString => Configuration.BuyingCatalogueConnectionString();
-
-    /// <summary>
-    /// Initialises a new instance of the <see cref="DbConnectionFactory"/> class.
-    /// </summary>
-    public DbConnectionFactory(IConfiguration configuration)
+    public sealed class DbConnectionFactory : IDbConnectionFactory
     {
-      Configuration = configuration ?? throw new System.ArgumentNullException(nameof(configuration));
+        private IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// Gets the database connection details.
+        /// </summary>
+        private string DefaultConnectionString => Configuration.BuyingCatalogueConnectionString();
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="DbConnectionFactory"/> class.
+        /// </summary>
+        public DbConnectionFactory(IConfiguration configuration)
+        {
+            Configuration = configuration ?? throw new System.ArgumentNullException(nameof(configuration));
+        }
+
+        /// <summary>
+        /// Gets a new database connection.
+        /// </summary>
+        /// <returns>A new database connection.</returns>
+        public async Task<IDbConnection> GetAsync(CancellationToken cancellationToken)
+        {
+            return await GetAsync(cancellationToken, new SqlConnectionStringBuilder(DefaultConnectionString));
+        }
+
+        /// <summary>
+        /// Gets a new database connection.
+        /// </summary>
+        /// <returns>A new database connection.</returns>
+        public async Task<IDbConnection> GetAsync(CancellationToken cancellationToken, DbConnectionStringBuilder connectionStringBuilder)
+        {
+            if (connectionStringBuilder is null)
+            {
+                throw new ArgumentNullException(nameof(connectionStringBuilder));
+            }
+
+            DbConnection connection = SqlClientFactory.Instance.CreateConnection();
+            connection.ConnectionString = connectionStringBuilder.ConnectionString;
+
+            await connection.OpenAsync(cancellationToken);
+
+            return connection;
+        }
     }
-
-    /// <summary>
-    /// Gets a new database connection.
-    /// </summary>
-    /// <returns>A new database connection.</returns>
-    public async Task<IDbConnection> GetAsync(CancellationToken cancellationToken)
-    {
-      DbConnection connection = SqlClientFactory.Instance.CreateConnection();
-      connection.ConnectionString = ConnectionString;
-
-      await connection.OpenAsync(cancellationToken);
-
-      return connection;
-    }
-  }
 }
