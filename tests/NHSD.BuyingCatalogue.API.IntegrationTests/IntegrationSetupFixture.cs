ab8729dea18e1817711aa408ca79940199681870
andy berry <andy.berry@bjss.com>
2019-09-26 12:10:44 +0100

    1793: Removed tests, refactored setup

--- tests/NHSD.BuyingCatalogue.API.IntegrationTests/IntegrationSetupFixture.cs
+++ tests/NHSD.BuyingCatalogue.API.IntegrationTests/IntegrationSetupFixture.cs
@@ -1,20 +1,27 @@
 using System.Threading.Tasks;
 using NHSD.BuyingCatalogue.API.IntegrationTests.Support;
-using NUnit.Framework;
+using NHSD.BuyingCatalogue.Testing.Data;
+using TechTalk.SpecFlow;
 
 namespace NHSD.BuyingCatalogue.API.IntegrationTests
 {
-    [SetUpFixture]
-    internal sealed class IntegrationSetupFixture
+    [Binding]
+    public class IntegrationSetupFixture
     {
-        [OneTimeSetUp]
-        public async Task OneTimeSetUpAsync()
+        [BeforeTestRun]
+        public static async Task OneTimeSetUpAsync()
         {
             await IntegrationTestEnvironment.StartAsync();
         }
 
-        [OneTimeTearDown]
-        public async Task OneTimeTearDownAsync()
+        [BeforeScenario()]
+        public static async Task SetUpAsync()
+        {
+            await Database.ClearAsync();
+        }
+
+        [AfterTestRun]
+        public static async Task OneTimeTearDownAsync()
         {
             await IntegrationTestEnvironment.StopAsync();
         }
