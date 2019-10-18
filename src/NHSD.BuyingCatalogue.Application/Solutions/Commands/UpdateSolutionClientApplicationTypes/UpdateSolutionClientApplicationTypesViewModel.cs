e82b72b2f25071f82bd63983b3c42006c4ecdb0e
Sam Roberts <53512773+sam-roberts-bjss@users.noreply.github.com>
2019-10-18 14:24:58 +0100

    3222: Match frontend request to update client application types

--- src/NHSD.BuyingCatalogue.Application/Solutions/Commands/UpdateSolutionClientApplicationTypes/UpdateSolutionClientApplicationTypesViewModel.cs
+++ src/NHSD.BuyingCatalogue.Application/Solutions/Commands/UpdateSolutionClientApplicationTypes/UpdateSolutionClientApplicationTypesViewModel.cs
@@ -1,25 +1,11 @@
+using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace NHSD.BuyingCatalogue.Application.Solutions.Commands.UpdateSolution
 {
     public sealed class UpdateSolutionClientApplicationTypesViewModel
     {
-        /// <summary>
-        /// Description of the solution.
-        /// </summary>
-        [JsonProperty("browser-based")]
-        public bool BrowserBased { get; set; }
-
-        /// <summary>
-        /// Summary of the solution.
-        /// </summary>
-        [JsonProperty("native-mobile")]
-        public string NativeMobile { get; set; }
-
-        /// <summary>
-        /// A link to more information regarding the solution.
-        /// </summary>
-        [JsonProperty("native-desktop")]
-        public string NativeDesktop { get; set; }
+        [JsonProperty("client-application-types")]
+        public HashSet<string> ClientApplicationTypes { get; set; }
     }
 }
