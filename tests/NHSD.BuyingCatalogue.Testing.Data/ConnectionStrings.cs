9fac195c5f4e97833ff5a62de11a122a1dcd8490
assoomro <adrian.soomro@gmail.com>
2019-10-21 09:52:45 +0100

    Explicitly specify the default port

--- tests/NHSD.BuyingCatalogue.Testing.Data/ConnectionStrings.cs
+++ tests/NHSD.BuyingCatalogue.Testing.Data/ConnectionStrings.cs
@@ -8,6 +8,6 @@ namespace NHSD.BuyingCatalogue.Testing.Data
 
         internal const string GPitFutures = @"Server={0};Initial Catalog=" + DataConstants.DatabaseName + ";Persist Security Info=False;User Id=NHSD;Password=DisruptTheMarket1!";
 
-        public static string ServiceConnectionString(string serverName = "localhost") => string.Format(GPitFutures, serverName);
+        public static string ServiceConnectionString(string serverName = DataConstants.ServerNameForTests) => string.Format(GPitFutures, serverName);
     }
 }
