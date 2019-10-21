808ebae01bb42f61ee67d7c4ad204013e7e7d48c
assoomro <adrian.soomro@gmail.com>
2019-10-21 09:52:45 +0100

    Explicitly specify the default port

--- tests/NHSD.BuyingCatalogue.Testing.Data/DataConstants.cs
+++ tests/NHSD.BuyingCatalogue.Testing.Data/DataConstants.cs
@@ -2,7 +2,7 @@ namespace NHSD.BuyingCatalogue.Testing.Data
 {
     public static class DataConstants
     {
-        internal const string ServerNameForTests = "localhost";
+        internal const string ServerNameForTests = "localhost,1433";
 
         public const string SAPassword = "Your_password456";
 
