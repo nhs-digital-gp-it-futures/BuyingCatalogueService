f9d8d8ce090770112352a5ac3171d8a8e1717c5d
andy berry <andy.berry@bjss.com>
2019-10-30 13:10:03 +0000

    3322: Refactored CustomExceptionFilter

--- src/NHSD.BuyingCatalogue.API/Infrastructure/Filters/CustomExceptionFilter.cs
+++ src/NHSD.BuyingCatalogue.API/Infrastructure/Filters/CustomExceptionFilter.cs
@@ -1,18 +1,16 @@
 using System;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using NHSD.BuyingCatalogue.Application.Exceptions;
+using NHSD.BuyingCatalogue.API.Extensions;
 
 namespace NHSD.BuyingCatalogue.API.Infrastructure.Filters
 {
-	/// <summary>
-	/// Provides a global exception filter.
-	/// </summary>
-	internal sealed class CustomExceptionFilter : IExceptionFilter
+    /// <summary>
+    /// Provides a global exception filter.
+    /// </summary>
+    internal sealed class CustomExceptionFilter : IExceptionFilter
 	{
         private readonly IWebHostEnvironment _webhostEnvironment;
         private readonly ILogger<CustomExceptionFilter> _logger;
@@ -32,52 +30,14 @@ namespace NHSD.BuyingCatalogue.API.Infrastructure.Filters
 		/// <param name="context">The Microsoft.AspNetCore.Mvc.Filters.ExceptionContext.</param>
 		public void OnException(ExceptionContext context)
 		{
-            _logger.LogError(new EventId(context.Exception.HResult), context.Exception, context.Exception.Message);
+            Exception exception = context.Exception;
 
-			Exception exception = context.Exception;
+            _logger.LogError(new EventId(context.Exception.HResult), exception, exception.Message);
 
-            object message;
-
-			if (_webhostEnvironment.IsDevelopment())
-			{
-				message = new
-				{
-					errors = new[] { "An unexpected error occurred." },
-					detail = exception.ToString()
-				};
-			}
-			else
-			{
-				message = new
-				{
-					errors = new[] { "An unexpected error occurred." },
-					detail = exception.Message
-				};
-			}
-
-			JsonResult json = new JsonResult(message);
-
-			context.HttpContext.Response.ContentType = "application/json";
-            context.HttpContext.Response.StatusCode = ConvertToStatusCode(exception);
-			context.Result = json;
+            context.HttpContext.Response.ContentType = "application/json";
+            context.HttpContext.Response.StatusCode = exception.ToStatusCode();
+			context.Result = exception.ToJsonMessage(_webhostEnvironment.IsDevelopment());
 			context.ExceptionHandled = true;
 		}
-
-        /// <summary>
-        /// Converts the specified exception into a HTTP status code. By default return <see cref="StatusCodes.Status500InternalServerError"/>.
-        /// </summary>
-        /// <param name="exception">Error details.</param>
-        /// <returns>The HTTP status code determined by the specified <paramref name="exception"/>.</returns>
-        private int ConvertToStatusCode(Exception exception)
-        {
-            int statusCode = StatusCodes.Status500InternalServerError;
-
-            if (exception is NotFoundException)
-            {
-                statusCode = StatusCodes.Status404NotFound;
-            }
-
-            return statusCode;
-        }
     }
 }
