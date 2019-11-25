7733b71527c5e3a1d7ddd13446543e284d3a35d7
andy berry <andy.berry@bjss.com>
2019-11-25 14:04:17 +0000

    3507: Self contain Integration test setup within this solution

--- tests/NHSD.BuyingCatalogue.Testing.Data/IntegrationDbSetup/build_image.sh
+++ tests/NHSD.BuyingCatalogue.Testing.Data/IntegrationDbSetup/build_image.sh
@@ -2,17 +2,7 @@
 
 set -e
 
-data_model_sql_resources="../../../../DataModel/SQL"
 integration_sql_resource="../SqlResources"
-
-if [ ! -d "$data_model_sql_resources" ]
-then
-    echo "[ ! ] Couldn't find the folder containing sql files for the integration tests"
-	echo "[ ! ] Expected to find the DataModel/SQL directory to be at $data_model_sql_resources"
-    echo "[ x ] Please make sure this directory exists and try again."
-    echo "[ ! ] Stopping the execution of setup-script."
-    exit 1
-fi
 
 # copy over sql file from the DataModel/SQL directory
 # copy over sql files from the SqlResources directory
@@ -23,7 +13,7 @@ then
 	mkdir SQL
 fi
 
-cp "$data_model_sql_resources"/"Create Database.sql" ./SQL/ && 
+cp "$integration_sql_resource"/"Create Database.sql" ./SQL/ && 
 cp -r "$integration_sql_resource"/. ./SQL/ && 
 docker build -f Dockerfile.build.integration . -t integration_db:test && 
 rm -rf ./SQL
