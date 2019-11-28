406bf50ae7cca0995d710132347a376f84dd7e00
Callum Bradbury <callum.bradbury@bjss.com>
2019-11-28 12:09:41 +0000

    Added integration tests for Dashboard Solution Contacts

--- tests/NHSD.BuyingCatalogue.API.IntegrationTests/Steps/Common/CommonSectionsSteps.cs
+++ tests/NHSD.BuyingCatalogue.API.IntegrationTests/Steps/Common/CommonSectionsSteps.cs
@@ -20,14 +20,14 @@ namespace NHSD.BuyingCatalogue.API.IntegrationTests.Steps
             _response = response;
         }
 
-        [Then(@"the solution (features|solution-description|client-application-types) section status is (COMPLETE|INCOMPLETE)")]
+        [Then(@"the solution (features|solution-description|client-application-types|contact-details) section status is (COMPLETE|INCOMPLETE)")]
         public async Task ThenTheSolutionSectionStatusIs(string section, string status)
         {
             var content = await _response.ReadBody();
             content.SelectToken($"sections.{section}.status").ToString().Should().Be(status);
         }
 
-        [Then(@"the solution (features|solution-description|client-application-types) section requirement is (Mandatory|Optional)")]
+        [Then(@"the solution (features|solution-description|client-application-types|contact-details) section requirement is (Mandatory|Optional)")]
         public async Task ThenTheSolutionSectionRequirementIsMandatory(string section, string requirement)
         {
             var content = await _response.ReadBody();
