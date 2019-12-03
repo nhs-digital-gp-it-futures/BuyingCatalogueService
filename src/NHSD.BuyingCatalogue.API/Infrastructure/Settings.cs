8fb338cff4b0f3d4196ec0b34c309ae49a03df4f
andy berry <andy.berry@bjss.com>
2019-12-03 15:23:49 +0000

    3528: Revert "3528: Hardcode connection string and explicitly map port" This reverts commit 054ff60ef96b4d7ea80688ac57f5dc0409305642.

--- src/NHSD.BuyingCatalogue.API/Infrastructure/Settings.cs
+++ src/NHSD.BuyingCatalogue.API/Infrastructure/Settings.cs
@@ -10,7 +10,6 @@ namespace NHSD.BuyingCatalogue.API.Infrastructure
 
         public Settings(IConfiguration configuration) => _configuration = configuration.ThrowIfNull();
 
-        public string ConnectionString =>
-            "Server=tcp:gpitfutures-sqlpri-dev.database.windows.net,1433;Initial Catalog=gpitfutures-db-dev;Persist Security Info=False;User ID=gpitfutureadmin;Password=6vygJRq9RhZy5knQw3WqkhPsPm77sujp;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"; //_configuration["ConnectionStrings:BuyingCatalogue"];
+        public string ConnectionString => _configuration["ConnectionStrings:BuyingCatalogue"];
     }
 }
