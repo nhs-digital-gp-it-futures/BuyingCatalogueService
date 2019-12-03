260497909a1ccb25cecdbdc383ae2f2a202a38b5
andy berry <andy.berry@bjss.com>
2019-12-03 15:42:38 +0000

    3528: Revert "3528: Hardcode connection string in DbConnection class" This reverts commit 13972298d250419fb15b58e0ddc85c45e703a140.

--- src/NHSD.BuyingCatalogue.Data/Infrastructure/DbConnectionFactory.cs
+++ src/NHSD.BuyingCatalogue.Data/Infrastructure/DbConnectionFactory.cs
@@ -34,10 +34,7 @@ namespace NHSD.BuyingCatalogue.Data.Infrastructure
         public async Task<IDbConnection> GetAsync(CancellationToken cancellationToken, DbConnectionStringBuilder connectionStringBuilder)
         {
             var connection = SqlClientFactory.Instance.CreateConnection();
-            //connection.ConnectionString = connectionStringBuilder.ThrowIfNull().ConnectionString;
-
-            connection.ConnectionString =
-                "Server=tcp:gpitfutures-sqlpri-dev.database.windows.net,1433;Initial Catalog=gpitfutures-db-dev;Persist Security Info=False;User ID=gpitfutureadmin;Password=6vygJRq9RhZy5knQw3WqkhPsPm77sujp;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            connection.ConnectionString = connectionStringBuilder.ThrowIfNull().ConnectionString;
 
             await connection.OpenAsync(cancellationToken);
 
