397eaa18a09fd666b0eadc2185d6a551a9e28784
Ruth Berry <ruth.berry@bjss.com>
2020-01-21 09:05:50 +0000

    3651-PremiseHostingType Validator change

--- src/NHSD.BuyingCatalogue.Solutions.Application/Commands/Hostings/OnPremise/UpdateOnPremiseValidator.cs
+++ src/NHSD.BuyingCatalogue.Solutions.Application/Commands/Hostings/OnPremise/UpdateOnPremiseValidator.cs
@@ -2,7 +2,7 @@ using NHSD.BuyingCatalogue.Solutions.Application.Commands.Validation;
 
 namespace NHSD.BuyingCatalogue.Solutions.Application.Commands.Hostings.OnPremise
 {
-    class UpdateOnPremiseValidator : IValidator<UpdateOnPremiseCommand, ISimpleResult>
+    internal sealed class UpdateOnPremiseValidator : IValidator<UpdateOnPremiseCommand, ISimpleResult>
     {
         public ISimpleResult Validate(UpdateOnPremiseCommand command)
             => new MaxLengthValidator()
