d1eb64cc42aedcd9e152aac941e996c3e433c546
Ruth Berry <ruth.berry@bjss.com>
2020-02-12 12:11:21 +0000

    3681: Epics test change

--- tests/NHSD.BuyingCatalogue.Solutions.API.UnitTests/EpicsControllerTests.cs
+++ tests/NHSD.BuyingCatalogue.Solutions.API.UnitTests/EpicsControllerTests.cs
@@ -42,7 +42,7 @@ namespace NHSD.BuyingCatalogue.Solutions.API.UnitTests
 
             var updateEpicsViewModel = new UpdateEpicsViewModel {ClaimedEpics = claimedEpics};
 
-            var epicsViewModel = new HashSet<IClaimedEpic>(updateEpicsViewModel?.ClaimedEpics);
+            var epicsViewModel = new HashSet<IClaimedEpic>(updateEpicsViewModel.ClaimedEpics);
 
             var viewModel = new UpdateEpicsViewModel {ClaimedEpics = claimedEpics};
             var validationModel = new Mock<ISimpleResult>();
@@ -52,12 +52,12 @@ namespace NHSD.BuyingCatalogue.Solutions.API.UnitTests
                 .Setup(m => m.Send(It.Is<UpdateEpicsCommand>(e => e.SolutionId == SolutionId && e.Data == epicsViewModel),
                     It.IsAny<CancellationToken>())).ReturnsAsync(validationModel.Object);
 
-            var result = (await _controller.Update(SolutionId, viewModel).ConfigureAwait(false)); //as NoContentResult;
+            var result = (await _controller.Update(SolutionId, viewModel).ConfigureAwait(false)) as NoContentResult;
             result.StatusCode.Should().Be((int)HttpStatusCode.NoContent);
 
             _mockMediator.Verify(
                 m => m.Send(It.Is<UpdateEpicsCommand>(e => e.SolutionId == SolutionId && e.Data == epicsViewModel),
-                    It.IsAny<CancellationToken>()), Times.Once);
+                    It.IsAny<CancellationToken>()));
         }
     }
 }
