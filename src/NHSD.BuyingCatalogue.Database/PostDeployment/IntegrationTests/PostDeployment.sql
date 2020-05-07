8e08d258e8fbdd11a12b745170b92436eab11df8
bjss-chris <christopher.martin@bjss.com>
2020-05-07 14:54:04 +0100

    Add integration test post-deployment script comment
    Added a comment to the integration test post-deployment script to make it clear that it is only executed when deploying the integration test environment (when the INTEGRATION_TEST environment variable is set to 'TRUE').
    
    AB#5417

--- src/NHSD.BuyingCatalogue.Database/PostDeployment/IntegrationTests/PostDeployment.sql
+++ src/NHSD.BuyingCatalogue.Database/PostDeployment/IntegrationTests/PostDeployment.sql
@@ -1,4 +1,7 @@
-﻿IF NOT EXISTS (SELECT * FROM information_schema.schemata WHERE schema_name = 'test')
+﻿-- This post-deployment script is only executed when deploying the integration test environment.
+-- i.e. When the INTEGRATION_TEST environment variable is set to 'TRUE'.
+
+IF NOT EXISTS (SELECT * FROM information_schema.schemata WHERE schema_name = 'test')
     EXEC sp_executesql N'CREATE SCHEMA test;'
 GO
 
