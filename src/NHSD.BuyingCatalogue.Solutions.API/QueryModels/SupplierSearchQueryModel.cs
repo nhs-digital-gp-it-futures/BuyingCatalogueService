7b7a5334b554c0b497af9b4683d80077a18c5673
bjss-chris <christopher.martin@bjss.com>
2020-06-04 16:35:04 +0100

    Fix supplier search query model
    AB#7465

---
+++ src/NHSD.BuyingCatalogue.Solutions.API/QueryModels/SupplierSearchQueryModel.cs
@@ -0,0 +1,11 @@
+﻿using NHSD.BuyingCatalogue.Solutions.Contracts;
+
+namespace NHSD.BuyingCatalogue.Solutions.API.QueryModels
+{
+    public sealed class SupplierSearchQueryModel
+    {
+        public string Name { get; set; }
+
+        public PublishedStatus? SolutionPublicationStatus { get; set; }
+    }
+}
