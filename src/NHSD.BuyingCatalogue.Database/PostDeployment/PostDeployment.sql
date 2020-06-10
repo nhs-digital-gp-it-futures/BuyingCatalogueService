9d5c37f63cef94da322444fa89fcf3585145a269
bjss-chris <christopher.martin@bjss.com>
2020-06-10 17:10:16 +0100

    Move common values to separate files
    AB#7621
    AB#7622

--- src/NHSD.BuyingCatalogue.Database/PostDeployment/PostDeployment.sql
+++ src/NHSD.BuyingCatalogue.Database/PostDeployment/PostDeployment.sql
@@ -1,4 +1,9 @@
-﻿:r ./InsertCommonValues.sql
+﻿:r ./InsertCapabilityStatuses.sql
+:r ./InsertCapabilityCategories.sql
+:r ./InsertCompliancyLevels.sql
+:r ./InsertPublicationStatuses.sql
+:r ./InsertSolutionCapabilityStatuses.sql
+:r ./InsertSolutionEpicStatuses.sql
 :r ./InsertCatalogueItemTypes.sql
 :r ./InsertFrameworks.sql
 :r ./InsertCapabilities.sql
