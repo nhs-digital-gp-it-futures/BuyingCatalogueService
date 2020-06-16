5687bc09244683320a7427588cf8da99146dc44b
Sam Roberts <53512773+sam-roberts-bjss@users.noreply.github.com>
2020-06-16 17:19:18 +0100

    Resolve PR comments
    AB#7260
    AB#7805

--- src/NHSD.BuyingCatalogue.Database/Tables/CataloguePrice.sql
+++ src/NHSD.BuyingCatalogue.Database/Tables/CataloguePrice.sql
@@ -1,14 +1,16 @@
 ﻿CREATE TABLE dbo.CataloguePrice
 (
     CataloguePriceId int IDENTITY(1,1) NOT NULL,
-    CatalogueItemId nvarchar(14) NOT NULL,
+    CatalogueItemId varchar(14) NOT NULL,
     ProvisioningTypeId int NOT NULL,
     CataloguePriceTypeId int NOT NULL,
     PricingUnitId uniqueidentifier NOT NULL,
     TimeUnitId int NULL,
-    CurrencyCode nvarchar(3) NOT NULL,
+    CurrencyCode varchar(3) NOT NULL,
     LastUpdated datetime2(7) NOT NULL,
+    Price decimal(18,3) NULL,
     CONSTRAINT PK_CataloguePrice PRIMARY KEY CLUSTERED (CataloguePriceId),
+    CONSTRAINT FK_CataloguePrice_CatalogueItem_CatalogueItemId FOREIGN KEY (CatalogueItemId) REFERENCES dbo.CatalogueItem(CatalogueItemId),
     CONSTRAINT FK_CataloguePrice_ProvisioningType_ProvisioningTypeId FOREIGN KEY (ProvisioningTypeId) REFERENCES dbo.ProvisioningType(ProvisioningTypeId),
     CONSTRAINT FK_CataloguePrice_CataloguePriceType_CataloguePriceTypeId FOREIGN KEY (CataloguePriceTypeId) REFERENCES dbo.CataloguePriceType(CataloguePriceTypeId),
     CONSTRAINT FK_CataloguePrice_PricingUnit_PricingUnitId FOREIGN KEY (PricingUnitId) REFERENCES dbo.PricingUnit(PricingUnitId),
