739f3f606cf5bbd09d620c5c1a381cec50b509c5
Ruth Berry <Ruth.berry@bjss.com>
2020-07-02 16:34:43 +0100

    5352: Canned data additional service

---
+++ src/NHSD.BuyingCatalogue.Solutions.API/Controllers/AdditionalServiceController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using NHSD.BuyingCatalogue.Solutions.API.ViewModels.AdditionalService;
+
+namespace NHSD.BuyingCatalogue.Solutions.API.Controllers
+{
+    [Route("api/v1/additional-services")]
+    [ApiController]
+    [Produces(MediaTypeNames.Application.Json)]
+    public sealed class AdditionalServiceController : ControllerBase
+    {
+        [HttpGet]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Cannot be viewed on swagger if static")]
+        public ActionResult<AdditionalServiceResult> Get([FromQuery] string[] solutionIds)
+        {
+            return new AdditionalServiceResult
+            {
+                Id = solutionIds.FirstOrDefault(),
+                Name = "Write on Time Additional Service 1",
+                Summary = "Addition to Write on Time",
+                Solution = new AdditionalServiceSolutionResult
+                {
+                    Id = "100000-001",
+                    Name = "Write on Time"
+                }
+            };
+        }
+    }
+}
---
+++ src/NHSD.BuyingCatalogue.Solutions.API/ViewModels/AdditionalService/AdditionalServiceResult.cs
@@ -0,0 +1,13 @@
+﻿namespace NHSD.BuyingCatalogue.Solutions.API.ViewModels.AdditionalService
+{
+    public sealed class AdditionalServiceResult
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Summary { get; set; }
+
+        public AdditionalServiceSolutionResult Solution { get; set; }
+    }
+}
