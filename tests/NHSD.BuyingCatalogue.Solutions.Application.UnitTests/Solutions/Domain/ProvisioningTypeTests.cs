0cd862e199ce56a6dcd8fb11cbcf0e3e1a57d315
stuart-bass-bjss <59572183+stuart-bass-bjss@users.noreply.github.com>
2020-07-06 09:06:05 +0100

    amended enum; added test

---
+++ tests/NHSD.BuyingCatalogue.Solutions.Application.UnitTests/Solutions/Domain/ProvisioningTypeTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using NHSD.BuyingCatalogue.Infrastructure;
+using NHSD.BuyingCatalogue.Solutions.Application.Domain.Pricing;
+using NUnit.Framework;
+
+namespace NHSD.BuyingCatalogue.Solutions.Application.UnitTests.Solutions.Domain
+{
+    [TestFixture]
+	public class ProvisioningTypeTests
+	{
+        [Test]
+        public void EnsureProvioningTypeNamingMatchesOrdapi()
+        {
+            //Arrange
+            var patient = Enumerator.FromName<ProvisioningType>("Patient");
+            var declarative = Enumerator.FromName<ProvisioningType>("Declarative");
+            var onDemand = Enumerator.FromName<ProvisioningType>("OnDemand");
+
+            //Assert
+            patient.Name.Should().Be("Patient");
+            declarative.Name.Should().Be("Declarative");
+            onDemand.Name.Should().Be("OnDemand");
+        }
+    }
+}
