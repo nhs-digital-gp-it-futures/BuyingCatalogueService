d0038b0e384cde1af28e95035c10bcbaf343b2a9
RahulJha-mastek <Rahul.Jha@mastek.com>
2021-03-03 16:50:24 +0000

    Update in Merge query

--- src/NHSD.BuyingCatalogue.Database/PostDeployment/InsertFrameworks.sql
+++ src/NHSD.BuyingCatalogue.Database/PostDeployment/InsertFrameworks.sql
@@ -1,8 +1,8 @@
 ﻿DECLARE @frameworks AS TABLE
 (
-    [Id]          NVARCHAR (10)  NOT NULL PRIMARY KEY,
-    [Name]        NVARCHAR (100) NOT NULL,
-    [Owner]       NVARCHAR (100) NULL
+    Id nvarchar(10) NOT NULL PRIMARY KEY,
+    [Name] nvarchar(100) NOT NULL,
+    [Owner] nvarchar(100) NULL
 );
 
 INSERT INTO @frameworks (Id, [Name], [Owner])
@@ -11,11 +11,10 @@ VALUES
 ('DFOCVC001', 'Digital First Online Consultation and Video Consultation Framework 1', 'NHS England');
 
 MERGE INTO dbo.Framework AS TARGET
-    USING @frameworks AS SOURCE
-        ON TARGET.Id = SOURCE.Id
-    WHEN MATCHED THEN
-        UPDATE SET TARGET.[Name] = SOURCE.[Name]
-    WHEN NOT MATCHED BY TARGET THEN
-        INSERT (Id, [Name], [Owner])
-        VALUES (SOURCE.[Id], SOURCE.[Name], SOURCE.[Owner]);
+     USING @frameworks AS SOURCE ON TARGET.Id = SOURCE.Id
+      WHEN MATCHED THEN
+           UPDATE SET TARGET.[Name] = SOURCE.[Name]
+      WHEN NOT MATCHED BY TARGET THEN
+           INSERT (Id, [Name], [Owner])
+           VALUES (SOURCE.Id, SOURCE.[Name], SOURCE.[Owner]);
 GO
