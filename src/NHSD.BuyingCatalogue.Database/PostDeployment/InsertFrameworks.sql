6d0c2bfc96c9180476e5be6a1136551b86c7bcb1
RahulJha-mastek <78419456+RahulJha-mastek@users.noreply.github.com>
2021-03-04 07:53:03 +0000

    Refactor InsertFrameworks script to use MERGE (#382)
    * Refactor InsertFrameworks script to use MERGE and insert new DFOCVC framework
    
    * Update in Merge query

--- src/NHSD.BuyingCatalogue.Database/PostDeployment/InsertFrameworks.sql
+++ src/NHSD.BuyingCatalogue.Database/PostDeployment/InsertFrameworks.sql
@@ -1,4 +1,20 @@
-﻿IF NOT EXISTS (SELECT * FROM dbo.Framework)
-    INSERT INTO dbo.Framework(Id, [Name], [Owner])
-    VALUES ('NHSDGP001', 'NHS Digital GP IT Futures Framework 1', 'NHS Digital');
+﻿DECLARE @frameworks AS TABLE
+(
+    Id nvarchar(10) NOT NULL PRIMARY KEY,
+    [Name] nvarchar(100) NOT NULL,
+    [Owner] nvarchar(100) NULL
+);
+
+INSERT INTO @frameworks (Id, [Name], [Owner])
+VALUES
+('NHSDGP001', 'NHS Digital GP IT Futures Framework 1', 'NHS Digital'),
+('DFOCVC001', 'Digital First Online Consultation and Video Consultation Framework 1', 'NHS England');
+
+MERGE INTO dbo.Framework AS TARGET
+     USING @frameworks AS SOURCE ON TARGET.Id = SOURCE.Id
+      WHEN MATCHED THEN
+           UPDATE SET TARGET.[Name] = SOURCE.[Name]
+      WHEN NOT MATCHED BY TARGET THEN
+           INSERT (Id, [Name], [Owner])
+           VALUES (SOURCE.Id, SOURCE.[Name], SOURCE.[Owner]);
 GO
